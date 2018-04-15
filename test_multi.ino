4585525647798146aeba3934ed2c36dcd98db9a6
Kushal Agrawal <kagrawal61@gmail.com>
2018-04-15 11:00:23 +0530

    changes Done

--- test_multi.ino
+++ test_multi.ino
@@ -4,11 +4,27 @@
 #define echoPin2 5
 #define trigPin3 7
 #define echoPin3 8
+
+#define BLYNK_PRINT SwSerial
+
+
+#include <SoftwareSerial.h>
+SoftwareSerial SwSerial(10, 11); // RX, TX
+    
+#include <BlynkSimpleStream.h>
+
+char auth[] = "c05a4ba70926468c95a412b8f170bafe";
+
+BlynkTimer timer;
+
+WidgetLED led1(V1);
+WidgetLED led2(V2);
  
 long duration, distance, RightSensor,BackSensor,FrontSensor,LeftSensor;
  
 void setup()
 {
+  SwSerial.begin(9600);
 Serial.begin (9600);
 pinMode(trigPin1, OUTPUT);
 pinMode(echoPin1, INPUT);
@@ -16,23 +32,21 @@ pinMode(trigPin2, OUTPUT);
 pinMode(echoPin2, INPUT);
 pinMode(trigPin3, OUTPUT);
 pinMode(echoPin3, INPUT);
+
+
+Blynk.begin(Serial, auth);
+timer.setInterval(1000L,Test);
+
 }
  
 void loop() {
-SonarSensor(trigPin1, echoPin1);
-RightSensor = distance;
-SonarSensor(trigPin2, echoPin2);
-LeftSensor = distance;
-SonarSensor(trigPin3, echoPin3);
-FrontSensor = distance;
- 
-//Serial.print(LeftSensor);
-//Serial.print(" - ");
-Serial.print(FrontSensor);
-Serial.print(" - ");
-Serial.println(RightSensor);
+
+Blynk.run();
+timer.run();
+
 }
  
+
 void SonarSensor(int trigPin,int echoPin)
 {
 digitalWrite(trigPin, LOW);
@@ -44,3 +58,37 @@ duration = pulseIn(echoPin, HIGH);
 distance = (duration/2) / 29.1;
  
 }
+
+void Test(){
+
+SonarSensor(trigPin1, echoPin1);
+RightSensor = distance;
+SonarSensor(trigPin2, echoPin2);
+LeftSensor = distance;
+SonarSensor(trigPin3, echoPin3);
+FrontSensor = distance;
+ 
+
+Serial.print("FrontSensor");
+Serial.println(FrontSensor);
+if(FrontSensor >=30){
+  led1.on();
+}
+else if(FrontSensor <30){
+  led1.off();
+}
+Serial.println();
+Serial.print("RightSensor");
+Serial.println(RightSensor);
+if(RightSensor >=30){
+  led2.on();
+}
+else if(RightSensor <30){
+  led2.off();
+}
+
+}
+
+
+
+
